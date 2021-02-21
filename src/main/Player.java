e3b991ee5dcc53b3456fb3b6f9463a553351a2f3
scen9 <scen9@gatech.edu>
2021-02-21 12:54:38 -0500

    Siying added "Player" class to the 1st version of "Initial_Game_Screen"

---
+++ src/main/Player.java
@@ -0,0 +1,37 @@
+package main;
+
+public class Player {
+
+    private String name;
+    private int health;
+    private int money;
+    private int level;
+
+    public Player() {
+        health = 100;
+        money = 0;
+        level = 1;
+        name = "";
+    }
+
+    // Edit by Siying: money varies based on difficulty
+    public void setMoney(int difficulty) {
+        if(difficulty == 1) {
+            money = 10000;
+        }
+        if(difficulty == 2) {
+            money = 20000;
+        }
+        if(difficulty == 3) {
+            money = 30000;
+        }
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
