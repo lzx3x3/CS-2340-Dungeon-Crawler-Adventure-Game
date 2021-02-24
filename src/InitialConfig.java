b55be4dac9313e7efe1053dd3e2bf42ddf0cc4da
Kayla Hunt <khunt37@gatech.edu>
2021-02-24 11:43:40 -0500

    Change name and difficulty inputs layout to centered

--- src/InitialConfig.java
+++ src/InitialConfig.java
@@ -1,9 +1,12 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,17 +19,21 @@ public class InitialConfig extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        VBox root = new VBox();
+        Label screenTitle = new Label("Start a New Game");
 
-        //enter name, select game difficulty
+        //enter name
         TextField nameInput = new TextField();
-        Label nameLabel = new Label("Enter your name: ");
+        nameInput.setPromptText("Enter your name");
+        nameInput.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+        Label nameLabel = new Label("Name:");
+        nameLabel.setPrefWidth(200);
 
-        ComboBox<String> difficulty = new ComboBox<>();
-        difficulty.getItems().addAll("Easy", "Medium", "Hard");
-        difficulty.getSelectionModel().selectLast();
-        difficulty.getSelectionModel().selectFirst();
-        Label diffLabel = new Label("Choose your difficulty level:");
+        //select your game difficulty
+        ComboBox<String> diffSelect = new ComboBox<>();
+        diffSelect.getItems().addAll("Easy", "Medium", "Hard");
+        diffSelect.setPromptText("Select your difficulty");
+        Label diffLabel = new Label("Difficulty:");
+        diffSelect.setPrefWidth(300);
 
 
         //player entries will be checked on button action
@@ -39,22 +46,34 @@ public class InitialConfig extends Application {
 //            nameAlert.showAndWait();
 //        }
 
-        HBox name = new HBox();
-        name.setAlignment(Pos.CENTER);
+        //Name label and text field
+        VBox name = new VBox();
+        name.setAlignment(Pos.CENTER_LEFT);
         name.getChildren().addAll(nameLabel, nameInput);
         name.setSpacing(10);
+        name.setPadding(new Insets(0, 50, 0, 50));
 
-        HBox diff = new HBox();
-        diff.setAlignment(Pos.CENTER);
-        diff.getChildren().addAll(diffLabel, difficulty);
-        diff.setSpacing(10);
+        //Difficulty label and drop down menu
+        VBox difficulty = new VBox();
+        difficulty.setAlignment(Pos.CENTER_LEFT);
+        difficulty.getChildren().addAll(diffLabel, diffSelect);
+        difficulty.setSpacing(10);
+        difficulty.setPadding(new Insets(0, 50, 0, 50));
 
-        root.setAlignment(Pos.CENTER);
-        root.setSpacing(10);
-        root.getChildren().addAll(name, diff);
+        //Everything that will go in the center of the border pane is together aligned in this VBOX
+        VBox selections = new VBox(name, difficulty);
+        selections.setSpacing(20);
 
-        Scene scene = new Scene(root, 400, 400);
-        primaryStage.setTitle("Initial Config Screen");
+        //Border pane with all selections in the center, the title in the Top, and the Start button can be in the bottom
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(screenTitle);
+        borderPane.setAlignment(screenTitle, Pos.BOTTOM_CENTER);
+        borderPane.setCenter(selections);
+        borderPane.setAlignment(selections, Pos.CENTER);
+
+        //The root will eventually be a stack pane so we can put everything on top of our background
+        Scene scene = new Scene(borderPane, 400, 400);
+        primaryStage.setTitle("Start a New Game");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
