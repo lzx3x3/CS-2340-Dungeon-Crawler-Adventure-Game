872392183295d945d98040ecfa550163e27dad19
scen9 <scen9@gatech.edu>
2021-02-27 14:52:09 -0500

    modifies InitialGameTest.java

--- tests/InitialGameTest.java
+++ tests/InitialGameTest.java
@@ -1,10 +1,8 @@
 import controller.Controller;
 import javafx.stage.Stage;
-import model.Player;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.matcher.base.NodeMatchers;
-import view.InitialGame;
 
 import static org.testfx.api.FxAssert.verifyThat;
 
@@ -28,7 +26,10 @@ public class InitialGameTest extends ApplicationTest {
         clickOn("BACK");
         verifyThat("Start a New Game", NodeMatchers.isNotNull());
     }
+
+
     //edited by Jessica
+    @Test
     public void testDifficulty() {
         clickOn("Start");
         write("test");
@@ -39,6 +40,7 @@ public class InitialGameTest extends ApplicationTest {
         verifyThat("Current Difficulty: Easy", NodeMatchers.isNotNull());
     }
 
+    @Test
     public void testStartingMoney() {
         clickOn("Start");
         write("test");
