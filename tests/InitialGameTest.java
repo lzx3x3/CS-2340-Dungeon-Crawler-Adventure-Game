9638ba2bd6b282c7f19ed83101c487870a64b0f7
jjacobs44 <jjacobs44@gatech.edu>
2021-02-27 14:27:30 -0500

    adds two test cases to InitialGameTest.java

--- tests/InitialGameTest.java
+++ tests/InitialGameTest.java
@@ -1,3 +1,4 @@
+import controller.Controller;
 import javafx.stage.Stage;
 import model.Player;
 import org.junit.Test;
@@ -9,19 +10,42 @@ import static org.testfx.api.FxAssert.verifyThat;
 
 public class InitialGameTest extends ApplicationTest {
 
-     private InitialGame initGame = new InitialGame(500, 500);
-     private Player player = new Player();
-
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(initGame.start(stage, player));
-        stage.show();
+        Controller controller = new Controller();
+        controller.start(stage);
     }
 
     //edited by Siying
     @Test
     public void testBack() {
-        clickOn("#BACK");
+        clickOn("Start");
+        write("test");
+        clickOn("Select your difficulty");
+        clickOn("Easy");
+        clickOn(".wpn1Button");
+        clickOn("Start");
+        clickOn("BACK");
         verifyThat("Start a New Game", NodeMatchers.isNotNull());
+    }
+    //edited by Jessica
+    public void testDifficulty() {
+        clickOn("Start");
+        write("test");
+        clickOn("Select your difficulty");
+        clickOn("Easy");
+        clickOn(".wpn1Button");
+        clickOn("Start");
+        verifyThat("Current Difficulty: Easy", NodeMatchers.isNotNull());
+    }
+
+    public void testStartingMoney() {
+        clickOn("Start");
+        write("test");
+        clickOn("Select your difficulty");
+        clickOn("Medium");
+        clickOn(".wpn1Button");
+        clickOn("Start");
+        verifyThat("Current Money: 20000", NodeMatchers.isNotNull());
     }
 }
