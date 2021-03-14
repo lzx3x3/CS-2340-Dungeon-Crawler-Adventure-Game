b95a6c99c8ff2c97cca0f6218a41f3b80fb52937
Kayla Hunt <khunt37@gatech.edu>
2021-03-13 21:30:56 -0500

    Fix checkstyle errors

--- src/controller/Draw.java
+++ src/controller/Draw.java
@@ -7,7 +7,7 @@ import model.Room;
 
 public class Draw {
 
-     public Pane draw(Player player) {
+    public Pane draw(Player player) {
         Pane root = new Pane();
         Room room = player.getCurrRoom();
         root = room.drawRoom(root, player);
