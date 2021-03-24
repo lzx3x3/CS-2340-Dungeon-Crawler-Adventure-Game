f6429afca9ec74ddf8747e080a21f585af5250d8
Kayla Hunt <khunt37@gatech.edu>
2021-03-23 21:40:46 -0400

    Fix Monster draw method header

--- src/controller/Draw.java
+++ src/controller/Draw.java
@@ -19,37 +19,37 @@ import model.StartRoom;
 
 public class Draw {
 
-//    public Pane draw(Player player, Controller controller) {
-//        Pane root = new Pane();
-//        Room room = player.getCurrRoom();
-//        root = room.drawRoom(root, player);
-//        if (player.getHealth() > 0) {
-//            root = player.drawPlayer(root);
-//            Text currentRoom = new Text("Current Room:"
-//                    + Integer.toString(player.getMaze().getX())
-//                    + Integer.toString(player.getMaze().getY()));
-//            currentRoom.setX(650);
-//            currentRoom.setY(400);
-//            root.getChildren().add(currentRoom);
-//        }
-//        else {
-//            root = player.drawDeadPlayer(root);
-////            ImageView image = new ImageView();
-////            image.setImage(new Image("file:resources/player.png"));
-////            Button restart = new Button("Try Again!", image);
-////            restart.setOnAction(e -> {
-////                controller.initInitialConfigScreen();
-////            });
-////            restart.setLayoutX(250);
-////            restart.setLayoutY(250);
-////
-////            Alert deadAlert = new Alert(Alert.AlertType.WARNING);
-////            deadAlert.setContentText("You died! Please click button 'Try Again'");
-////            deadAlert.show();
-////            root.getChildren().add(restart);
-//        }
-//        return root;
-//    }
+    public Pane draw(Player player, Controller controller) {
+        Pane root = new Pane();
+        Room room = player.getCurrRoom();
+        root = room.drawRoom(root, player);
+        if (player.getHealth() > 0) {
+            root = player.drawPlayer(root);
+            Text currentRoom = new Text("Current Room:"
+                    + Integer.toString(player.getMaze().getX())
+                    + Integer.toString(player.getMaze().getY()));
+            currentRoom.setX(650);
+            currentRoom.setY(400);
+            root.getChildren().add(currentRoom);
+        }
+        else {
+            root = player.drawDeadPlayer(root);
+//            ImageView image = new ImageView();
+//            image.setImage(new Image("file:resources/player.png"));
+//            Button restart = new Button("Try Again!", image);
+//            restart.setOnAction(e -> {
+//                controller.initInitialConfigScreen();
+//            });
+//            restart.setLayoutX(250);
+//            restart.setLayoutY(250);
+//
+//            Alert deadAlert = new Alert(Alert.AlertType.WARNING);
+//            deadAlert.setContentText("You died! Please click button 'Try Again'");
+//            deadAlert.show();
+//            root.getChildren().add(restart);
+        }
+        return root;
+    }
 
     public Pane drawMonster1(Player player, Controller controller, Monster1 monster1) {
         Pane root = new Pane();
