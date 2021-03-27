ea49e9ff59cbe6dc7ec50c80e04bec1cd10578ec
scen9 <scen9@gatech.edu>
2021-03-27 18:23:45 -0400

    add tests in AttackTest.java

--- tests/AttackTest.java
+++ tests/AttackTest.java
@@ -37,4 +37,33 @@ public class AttackTest extends ApplicationTest {
         type(KeyCode.S, 6);
         FxAssert.verifyThat("#monHealth", TextMatchers.hasText("health: 100"));
     }
+
+    // Edited by Siying
+    @Test
+    public void testMonsterHealthAttackLeftCloser() {
+        type(KeyCode.S, 7);
+        type(KeyCode.S, 6);
+        type(KeyCode.D, 1);
+        type(KeyCode.F, 1);
+        FxAssert.verifyThat("#monHealth", TextMatchers.hasText("health: 80"));
+    }
+
+    // Edited by Siying
+    @Test
+    public void testMonsterHealthAttackLeftFurther() {
+        type(KeyCode.S, 7);
+        type(KeyCode.S, 6);
+        type(KeyCode.F, 1);
+        FxAssert.verifyThat("#monHealth", TextMatchers.hasText("health: 99"));
+    }
+
+    // Edited by Siying
+    @Test
+    public void testMonsterHealthAttackUp() {
+        type(KeyCode.S, 7);
+        type(KeyCode.S, 4);
+        type(KeyCode.D, 2);
+        type(KeyCode.F, 1);
+        FxAssert.verifyThat("#monHealth", TextMatchers.hasText("health: 80"));
+    }
 }
